0b0d1ff3b524da3da94a183af3653e14240fcb92
thrila <ifeanyichukwundubuizu@gmail.com>
2023-01-13 01:42:19 +0100

    ADD - design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
 function App() {
+  const [foodDetails , setFoodDetails] = useState({})
+  const URL = `https://api.nal.usda.gov/fdc/v1/foods/search?query=rice&pageSize=6&api_key=${process.env.REACT_APP_FOOD_KEY}`;
+              setInterval(() => {
+                axios.get(URL)
+              .then((res)=> {
+                const result = res.data;
+                console.log(result);
+                setFoodDetails(result);
+              })
+              .catch((err)=> {
+                console.log(err);
+              })
+              }, 3000);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="search">
+        <input type="text" placeholder="Enter food ..." />
+      </div>
+
+      {foodDetails ? foodDetails.map((item)=> { <h1>{item.currentPage} </h1> }) : 'help'}
+
+
+      {/* <div className="container">
+        <div className="card">
+          <div className="top">
+            <div className="brand">
+              <h1>Nelly foods</h1>
+            </div>
+            <div className="description">
+              <p>
+                this is the description for nelly's rice more more text and
+                stuff
+              </p>
+            </div>
+            <div className="food-catergory">
+              <p>flovoured foods</p>
+            </div>
+          </div>
+          <div className="buttom">
+            <ul>
+              <li>
+                <p>Protein</p>
+              </li>
+              <li>
+                <p>Carbs</p>
+              </li>
+              <li>
+                <p>Fats</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 }
